a60f8a0d9b0b8c354f1b31eca6ce5cddf4659ec7
vikctar <vikc.steve@gmail.com>
2016-09-03 22:20:55 +0300

    Find views by their ids

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -17,6 +17,7 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +27,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find the view that shows the numbers category
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        // Find the view that shows the family members category
+        TextView family = (TextView) findViewById(R.id.family);
+
+        // Find the view that shows the colors category
+        TextView colors = (TextView) findViewById(R.id.colors);
+
+        // Find the view that shows the phrases category
+        TextView phrases = (TextView) findViewById(R.id.phrases);
     }
 }
