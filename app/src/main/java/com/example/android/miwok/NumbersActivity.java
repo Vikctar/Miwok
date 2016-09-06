9679d05d333e62cfd2ea91aae6cfdce7ddcdfe8b
vikctar <vikc.steve@gmail.com>
2016-09-06 16:16:11 +0300

    Some good code practice

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,6 @@ package com.example.android.miwok;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -33,12 +32,17 @@ public class NumbersActivity extends AppCompatActivity {
 
         LinearLayout rootView = (LinearLayout) findViewById(R.id.root_view);
 
+        // Create a variable to keep track of current index position
         int index = 0;
 
         while (index < words.size()) {
+            // Create a new {@link TextView} that displays word at index
+            // and add the View as a child to the rootView
             TextView wordView = new TextView(this);
             wordView.setText(words.get(index));
-            rootView.addView(wordView);
+            if (rootView != null) {
+                rootView.addView(wordView);
+            }
 
             index += 1;
         }
